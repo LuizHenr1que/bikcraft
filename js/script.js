413ee75c34ff26878964fabe05ef11f2337ab8e6
Luiz Henrique <Luiizhenrique97444@gmail.com>
2023-04-26 13:44:06 -0300

    Parâmetros URL

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-const links = document.querySelectorAll('.header-menu a');
+// Ativar Links do Menu
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-    const url = window.location.href;
-    const href = link.href;
-
-    url.includes
-    if(url.includes(href)) {
-        link.classList.add('ativo')
-    }
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
